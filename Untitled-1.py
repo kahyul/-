e38e980f09ce4f3ffca55ff9072a2e8725ef4a8b
kahyul <97814033+kahyul@users.noreply.github.com>
2022-01-16 09:45:20 +0900

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,21 @@
+import discord
+token = 'OTMxODg3OTIxMzI4NDUxNTg0.YeK-Qg.b3rXaiiJ9CIsW123MVT8q1YjfXY'
+client = discord.Client()
+@client.event
+async def on_ready():
+    print('로그인되었습니다!')
+    print(client.user.name)
+    print(client.user.id)
+    print('====================================')
+
+@client.event
+async def on_ready(): # 봇이 실행되면 한 번 실행됨
+    print("이 문장은 Python의 내장 함수를 출력하는 터미널에서 실행됩니다\n지금 보이는 것 처럼 말이죠")
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("채편님 팬이에요"))
+
+@client.event
+async def on_message(message):
+    if message.content == '심채편':
+        await message.channel.send('잘생김!')
+
+client.run(token)
